45186d3511765fa72a4013d746fffe21a22a80ef
Wedsa95 <JonasOlsson1995@gmail.com>
2018-01-21 16:39:35 +0100

    First commit

---
+++ SpringBootDataList/src/main/java/com/olsson/controller/VisitorController.java
@@ -0,0 +1,30 @@
+package com.olsson.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.olsson.model.Visitor;
+import com.olsson.repository.VisitorRepository;
+
+@Controller
+public class VisitorController {
+
+    @Autowired
+    VisitorRepository visitorRepository;
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("visitors", visitorRepository.findAll());
+        return "home";
+    }
+
+    @RequestMapping(value = "/addVisitor", method = RequestMethod.POST)
+    public String addCar(@ModelAttribute Visitor visitor) {
+        visitorRepository.save(visitor);
+        return "redirect:home";
+    }
+}
---
+++ SpringBootDataList/src/main/java/com/olsson/repository/VisitorRepository.java
@@ -0,0 +1,9 @@
+package com.olsson.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.olsson.model.Visitor;
+
+public interface VisitorRepository extends CrudRepository<Visitor, String>{
+
+}
